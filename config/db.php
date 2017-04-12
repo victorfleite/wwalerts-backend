60f541ef8c3b0afd2bc2baec0ee7d859cdddc160
Victor Leite <victor.leite@inmet.gov.br>
2017-04-12 15:02:10 -0300

    reinicialização de projeto

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=alerts',
-    'username' => 'postgres',
-    'password' => 'postgres',
-    'charset' => 'utf8',
-];
