9d0f9c84aedcc5210689fdea1148fa3d356e1bb4
victor.leite <victor.leite@inmet.gov.br>
2017-05-15 14:55:19 -0300

    Removidos Classes estranhas ao projeto

--- service/api/versions/v1/models/common/User.php
+++ service/api/versions/v1/models/common/User.php
@@ -7,26 +7,6 @@ use Yii;
 /**
  * This is the model class for table "user".
  *
- * @property integer $id
- * @property string $username
- * @property string $email
- * @property string $password_hash
- * @property string $auth_key
- * @property integer $confirmed_at
- * @property string $unconfirmed_email
- * @property integer $blocked_at
- * @property string $registration_ip
- * @property integer $created_at
- * @property integer $updated_at
- * @property integer $flags
- * @property integer $status
- * 
- * 
- */
-
-/**
- * This is the model class for table "user".
- *
  */
 class User extends \yii\db\ActiveRecord
 {
@@ -38,12 +18,6 @@ class User extends \yii\db\ActiveRecord
         return 'user';
     }
 
-    /**
-    * @return \yii\db\ActiveQuery
-    */
-    public function getProfile()
-    {
-        return $this->hasOne(Profile::className(), ['user_id' => 'id']);
-    }
+
 
 }
