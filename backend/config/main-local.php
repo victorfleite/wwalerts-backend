d3c7646d6ac6441e8ce2e19840ef3e0ae9629a82
victor.leite <victor.leite@inmet.gov.br>
2017-05-19 16:08:51 -0300

    Adiciado arquivos locais

---
+++ backend/config/main-local.php
@@ -0,0 +1,30 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'sRa3v_GrHvZbufz5z1Dkyji9KxvyhCkN',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module'
+    ];
+    $config['modules']['debug']['allowedIPs'] = ['*'];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+    $config['modules']['gii']['allowedIPs'] = ['*'];
+
+
+}
+
+
+return $config;
