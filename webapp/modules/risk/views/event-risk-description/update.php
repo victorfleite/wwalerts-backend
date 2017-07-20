22be02755d3cf2af644e1c489d4cc5cb2edbed52
victor.leite <victor.leite@inmet.gov.br>
2017-07-20 11:48:10 -0300

    Atualizando vizualização

--- webapp/modules/risk/views/event-risk-description/update.php
+++ webapp/modules/risk/views/event-risk-description/update.php
@@ -5,11 +5,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model webapp\modules\risk\models\EventRiskDescription */
 
-$this->title = Yii::t('translation', 'Update {modelClass}: ', [
-    'modelClass' => 'Event Risk Description',
-]) . $model->id;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('translation', 'Event Risk Descriptions'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->title = Yii::t('translation', 'update_title', [
+    'name' => $model->name_i18n,
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('translation', 'event_risk_descriptions'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name_i18n, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('translation', 'Update');
 ?>
 <div class="event-risk-description-update">
--- webapp/modules/risk/views/event-risk-description/view.php
+++ webapp/modules/risk/views/event-risk-description/view.php
@@ -4,44 +4,69 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 use common\components\widgets\datailview_i18n\DatailViewI18n;
 
-
 /* @var $this yii\web\View */
 /* @var $model webapp\modules\risk\models\EventRiskDescription */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('translation', 'Event Risk Descriptions'), 'url' => ['index']];
+$this->title = $model->name_i18n;
+$this->params['breadcrumbs'][] = ['label' => $model->name_i18n, 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="event-risk-description-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Yii::t('translation', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('translation', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('translation', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
+    <p class="text-right">
+	<?= Html::a(Yii::t('translation', 'Admin'), ['index'], ['class' => 'btn btn-primary']) ?>
+	<?= Html::a(Yii::t('translation', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+	<?=
+	Html::a(Yii::t('translation', 'Delete'), ['delete', 'id' => $model->id], [
+	    'class' => 'btn btn-danger',
+	    'data' => [
+		'confirm' => Yii::t('translation', 'Are you sure you want to delete this item?'),
+		'method' => 'post',
+	    ],
+	])
+	?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name_i18n',
-            'created_at',
-            'updated_at',
-            'created_by',
-            'updated_by',
-            'risk_id',
-            'event_id',
-            'hash',
-        ],
-    ]) ?>
-    
+    <?=
+    DetailView::widget([
+	'model' => $model,
+	'attributes' => [
+	    'name_i18n',
+	    [
+		'attribute' => 'event_id',
+		'value' => function($data) {
+		    return Yii::t('translation', $data->event->name_i18n);
+		},
+	    ],
+		[
+		'attribute' => 'risk_id',
+		'value' => function($data) {
+		    return Yii::t('translation', $data->risk->name_i18n);
+		},
+	    ],
+	    'created_at:datetime',
+	    'updated_at:datetime',
+		[
+		'attribute' => 'created_by',
+		'value' => function($data) {
+		    $user = \common\models\User::findOne($data->created_by);
+		    return $user->name;
+		},
+	    ],
+		[
+		'attribute' => 'updated_by',
+		'value' => function($data) {
+		    $user = \common\models\User::findOne($data->updated_by);
+		    return $user->name;
+		},
+	    ],
+	    'hash',
+	],
+    ])
+    ?>
+
     <h2><?= Yii::t('translation', 'translations') ?></h2>
 
     <?=
